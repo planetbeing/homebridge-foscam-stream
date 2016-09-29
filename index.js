c61dee690b3364b26662e780c2ea5e7ca37dadd9
David Wang <planetbeing@gmail.com>
2016-09-28 18:07:50 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = {
+    FoscamAccessory: require('./lib/FoscamAccessory'),
+    FoscamStream: require('./lib/FoscamStream'),
+    RTPAudioTranscoder: require('./lib/RTPAudioTranscoder'),
+    RTPG711Transcoder: require('./lib/RTPG711Transcoder'),
+    RTSPClient: require('./lib/RTSPClient')
+};
